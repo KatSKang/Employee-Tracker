5acc028d10d85b57c434f746e6df566cdd55c715
Kathy Kang <105574653+KatSKang@users.noreply.github.com>
2022-09-22 14:32:24 -0400

    Add menu questions, create db dir, add packages

---
+++ index.js
@@ -0,0 +1,46 @@
+const sql = require('mysql12');
+const inquirer = require('inquirer');
+
+
+const menuQuestions = [
+    {
+        type: 'list',
+        choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role', 'Close app'],
+        message: 'What would you like to do?'
+    }
+];
+
+
+// Function for menu questions 
+function mainMenu() {
+    inquirer.prompt(menuQuestions)
+        .then((res) => {
+            switch(res.menu){
+                case "View all departments":
+                    return viewAllDepartments();
+                case "View all roles":
+                    return addIntern();
+                case "View all employees'":
+                    return ;
+                case "Add a department":
+                    return ;
+                case "Add a role":
+                    return ;
+                case "Add an employee":
+                    return ;
+                case "Update an employee role":
+                    return ;
+                case "Close app":
+                    return;
+            }
+        })
+}
+
+// Call function to initialize app
+mainMenu();
+
+
+function viewAllDepartments() {
+    
+}
+
